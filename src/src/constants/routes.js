1d58e681d40dc2c671973e3c3aed41eaa78d4aa7
j8won <joowoni99@gmail.com>
2023-09-26 21:19:09 +0900

    init: 프로젝트 초기 설정

---
+++ src/src/constants/routes.js
@@ -0,0 +1,6 @@
+const routes = {
+  home: "/",
+};
+
+Object.freeze(routes);
+export default routes;
---
+++ src/src/styles/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+  color: {
+    black: "#222222",
+  },
+
+  fontFamily: {
+    main: '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif',
+  },
+
+  border: {},
+};
+
+Object.freeze(theme);
+export default theme;
