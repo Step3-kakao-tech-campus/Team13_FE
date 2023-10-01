f9919115f878f2b1c47b24e17cd2951aa6d8c2c3
j8won <joowoni99@gmail.com>
2023-10-01 15:50:20 +0900

    feat: routes.js에 펀딩 페이지, 셀럽 페이지 uri 추가

--- src/src/constants/routes.js
+++ src/src/constants/routes.js
@@ -1,5 +1,7 @@
 const routes = {
   home: "/",
+  fund: "/fund",
+  celebrity: "/celebrity",
 };
 
 Object.freeze(routes);
--- src/src/router.jsx
+++ src/src/router.jsx
@@ -2,16 +2,25 @@ import { createBrowserRouter } from "react-router-dom";
 
 import routes from "@/constants/routes.js";
 import Home from "@/pages/Home.jsx";
+import Layout from "@/components/common/layout/Layout.jsx";
 
 const router = createBrowserRouter([
   {
     path: "",
-    // element: ,
+    element: <Layout />,
     // loader: ,
     // errorElement: ,
     children: [
       {
         path: routes.home,
+        element: <Home />,
+      },
+      {
+        path: routes.fund,
+        element: <Home />,
+      },
+      {
+        path: routes.celebrity,
         element: <Home />,
       },
     ],
