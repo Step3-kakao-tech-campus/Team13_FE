0bba5956d6ab3089100bdf56dd97d011bcea84ff
j8won <joowoni99@gmail.com>
2023-10-02 14:42:21 +0900

    feat: SearchIcon 생성 및 다른 아이콘 수정

---
+++ src/src/assets/SearchIcon.jsx
@@ -0,0 +1,20 @@
+import { memo } from "react";
+
+function SearchIcon() {
+  return (
+    <>
+      <img
+        width="36"
+        height="36"
+        src="https://img.icons8.com/material-rounded/100/4a4a4a/search.png"
+        alt="search"
+      />
+      <div id="license" style={{ display: "none" }}>
+        <a href="https://icons8.com/icon/83218/search">Search</a> icon by{" "}
+        <a href="https://icons8.com">Icons8</a>
+      </div>
+    </>
+  );
+}
+
+export default memo(SearchIcon);
