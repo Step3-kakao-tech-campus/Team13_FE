dbe6c4af4dfe2008f3090131a7f8abc5ba33d34a
sihyonn <sihyeon982@naver.com>
2023-10-04 00:02:07 +0900

    feat: LoginPage Form.jsx styled-components 적용, UI완성

--- src/src/pages/LoginPage.jsx
+++ src/src/pages/LoginPage.jsx
@@ -1,6 +1,40 @@
+import { useNavigate } from "react-router-dom";
+import routes from "@/constants/routes.js";
+import styled from "styled-components";
+
+import Button from "@/components/common/Button.jsx";
 import Form from "@/components/common/form/Form.jsx";
 
+const Styled = {
+  LoginContainer: styled.div`
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+    margin: 9rem 0;
+  `,
+  LoginTitle: styled.h1`
+    font-size: 1.75rem;
+    font-weight: bold;
+    text-align: left;
+  `,
+  BottomMsg: styled.div`
+    font-size: 0.75rem;
+    display: flex;
+    justify-content: center;
+    padding: 0.75rem;
+
+    & span {
+      color: ${({ theme }) => theme.color.mainRed};
+      display: block;
+      margin-left: 0.25rem;
+      cursor: pointer;
+    }
+  `,
+};
+
 function LoginPage() {
+  const navigate = useNavigate();
+
   const loginDefault = {
     email: "",
     password: "",
@@ -9,7 +43,6 @@ function LoginPage() {
   const loginInfo = [
     {
       id: "email",
-      // label: "이메일",
       type: "text",
       placeholder: "kakao@jnu.ac.kr",
       validation: {
@@ -35,19 +68,50 @@ function LoginPage() {
         minLength: { value: 8, message: "비밀번호는 8글자 이상입니다" },
         maxLength: { value: 20, message: "비밀번호는 20글자 이내입니다" },
       },
-      // requireMsg: "8~20자 공백 없이 영문, 숫자, 특수문자를 포함해주세요!",
     },
   ];
 
   return (
-    <div>
-      <Form
-        onSubmit={(data) => console.log(data)}
-        onError={(err) => console.log(err)}
-        inputInformations={loginInfo}
-        defaultValues={loginDefault}
-      />
-    </div>
+    <Styled.LoginContainer>
+      <div
+        style={{
+          width: "22rem",
+        }}
+      >
+        <Styled.LoginTitle>로그인</Styled.LoginTitle>
+        <Form
+          onSubmit={(data) => console.log(data)}
+          onError={(err) => console.log(err)}
+          inputInformations={loginInfo}
+          defaultValues={loginDefault}
+        />
+        <Button
+          style={{
+            width: "100%",
+            padding: "1rem",
+            margin: "0.375rem 0",
+          }}
+        >
+          로그인
+        </Button>
+        <Button
+          style={{
+            width: "100%",
+            padding: "1rem",
+            margin: "0.375rem 0",
+            backgroundColor: "#FFEB02",
+            color: "#000000",
+            fontWeight: "bold",
+          }}
+        >
+          카카오 간편 로그인
+        </Button>
+        <Styled.BottomMsg>
+          아직 펀더링 계정이 없으신가요?
+          <span onClick={() => navigate(routes.signUp)}>회원가입</span>
+        </Styled.BottomMsg>
+      </div>
+    </Styled.LoginContainer>
   );
 }
 
