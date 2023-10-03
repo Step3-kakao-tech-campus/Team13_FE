1c72e9e4edff042303b9b3972cd3057fee7c4904
sihyonn <sihyeon982@naver.com>
2023-10-04 00:00:37 +0900

    feat: Form.jsx styled-components, PropTypes 적용

--- src/src/components/common/form/Form.jsx
+++ src/src/components/common/form/Form.jsx
@@ -1,5 +1,11 @@
 import { useForm, FormProvider } from "react-hook-form";
-import LabeledInput from "@/components/common/form/LabeledInput";
+import { PropTypes } from "prop-types";
+import styled from "styled-components";
+import LabeledInput from "@/components/common/form/LabeledInput.jsx";
+
+const Styled = {
+  Form: styled.form``,
+};
 
 function Form({
   onSubmit,
@@ -8,7 +14,7 @@ function Form({
   inputInformations,
   children,
 }) {
-  const methods = useForm({ mode: "onTouched", defaultValues: defaultValues });
+  const methods = useForm({ mode: "onChange", defaultValues: defaultValues });
   const {
     handleSubmit,
     formState: { errors },
@@ -16,12 +22,13 @@ function Form({
 
   return (
     <FormProvider {...methods}>
-      <form onSubmit={handleSubmit(onSubmit, onError)}>
+      <Styled.Form onSubmit={handleSubmit(onSubmit, onError)}>
         {inputInformations.map((input) => (
           <LabeledInput
             key={input.id}
             id={input.id}
             type={input.type}
+            label={input.label}
             placeholder={input.placeholder}
             validation={input.validation}
             errorMsg={errors?.[input.id]?.message}
@@ -29,9 +36,33 @@ function Form({
           />
         ))}
         {children}
-      </form>
+      </Styled.Form>
     </FormProvider>
   );
 }
+
+Form.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onError: PropTypes.func,
+  defaultValues: PropTypes.oneOfType([PropTypes.object, PropTypes.func])
+    .isRequired,
+  inputInformations: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      placeholder: PropTypes.string.isRequired,
+      validation: PropTypes.shape({
+        required: PropTypes.string,
+        pattern: PropTypes.shape({
+          value: PropTypes.any,
+          message: PropTypes.string,
+        }),
+      }),
+      requireMsg: PropTypes.string,
+    })
+  ),
+  style: PropTypes.object,
+  children: PropTypes.node,
+};
 
 export default Form;
