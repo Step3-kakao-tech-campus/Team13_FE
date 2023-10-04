afc3c61e585448a37785c6db4445f99fbe5f88db
j8won <joowoni99@gmail.com>
2023-10-05 02:49:34 +0900

    feat: 찜하기 컴포넌트 HeartButton 생성

---
+++ src/src/assets/icon/FilledHeartIcon.jsx
@@ -0,0 +1,18 @@
+function FilledHeartIcon() {
+  return (
+    <>
+      <img
+        width="24"
+        height="24"
+        src="https://img.icons8.com/ios-filled/100/FF6C6C/like--v1.png"
+        alt="like--v1"
+      />
+      <div id="license" style={{ display: "none" }}>
+        <a href="https://icons8.com/icon/7697/heart">Heart</a> icon by{" "}
+        <a href="https://icons8.com">Icons8</a>
+      </div>
+    </>
+  );
+}
+
+export default FilledHeartIcon;
