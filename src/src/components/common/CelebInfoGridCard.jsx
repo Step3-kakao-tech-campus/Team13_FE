3307553e71b39405762efcf8b8aff8a2e85665f8
Klomachenko <leekyumin0901@naver.com>
2023-10-05 02:05:21 +0900

    feat : CelebInfoGridCard 셀럽페이지 컴포넌트 생성

---
+++ src/src/components/common/CelebInfoGridCard.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import { PropTypes } from 'prop-types';
+import SignUpIcon from '@/assets/SignUpIcon.jsx';
+
+const Styled = {};
+
+function CelebInfoGridCard({ children, ...props }) {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+CelebInfoGridCard.propTypes = {
+  children: PropTypes.node,
+};
+
+export default CelebInfoGridCard;
