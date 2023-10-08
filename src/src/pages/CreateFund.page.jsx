674743a6470f67d8584c5d275a6c70f42768186f
j8won <joowoni99@gmail.com>
2023-10-08 21:23:55 +0900

    feat: 펀딩 주최하기 페이지 펀딩 제목 autosize textarea 추가

--- src/src/pages/CreateFund.page.jsx
+++ src/src/pages/CreateFund.page.jsx
@@ -1,10 +1,54 @@
 import styled from "styled-components";
+import TextareaAutoSize from "react-textarea-autosize";
+
 import { Title } from "@/styles/CommonStyle";
+import ThumbnailBox from "@/components/create-fund/ThumbnailBox.jsx";
 
 const Styled = {
   Container: styled.section`
     margin: 3rem 0;
     padding: 0 calc((100% - 41.5rem) / 2);
+  `,
+  InputContainer: styled.article`
+    margin: 2rem 0;
+
+    padding: 2rem;
+    width: 100%;
+    background-color: ${({ theme }) => theme.color.white};
+    border: ${({ theme }) => theme.border.main};
+    border-radius: 0.25rem;
+  `,
+  TitleInput: styled(TextareaAutoSize)`
+    padding: 0 0.5rem 0.5rem;
+    width: 100%;
+    resize: none;
+
+    font-size: 1.5rem;
+    color: ${({ theme }) => theme.color.body};
+
+    overflow-wrap: break-word;
+    word-break: break-all;
+    white-space: pre-wrap;
+
+    border: none;
+    border-bottom: 2px solid ${({ theme }) => theme.color.inactive};
+    outline: none;
+
+    transition: border-bottom-color ease-in-out 0.3s;
+
+    &::placeholder {
+      color: ${({ theme }) => theme.color.addition};
+    }
+
+    &:focus {
+      border-bottom: 2px solid ${({ theme }) => theme.color.mainRed};
+    }
+  `,
+  Subtitle: styled.div`
+    margin-bottom: 1rem;
+    font-size: 1.25rem;
+    font-weight: 500;
+    color: ${({ theme }) => theme.color.body};
   `,
 };
 
@@ -12,6 +56,12 @@ function CreateFundPage() {
   return (
     <Styled.Container>
       <Title>펀딩 주최하기</Title>
+      <Styled.InputContainer>
+        <Styled.TitleInput placeholder="펀딩 제목을 입력해 주세요" />
+      </Styled.InputContainer>
+
+      <Styled.Subtitle>썸네일</Styled.Subtitle>
+      <ThumbnailBox />
     </Styled.Container>
   );
 }
