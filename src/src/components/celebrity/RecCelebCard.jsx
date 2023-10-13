fae8a2792029afafe9a616e919862d4e8da0d060
sihyonn <sihyeon982@naver.com>
2023-10-14 00:30:23 +0900

    style: 추천셀럽카드 스타일링

--- src/src/components/celebrity/RecCelebCard.jsx
+++ src/src/components/celebrity/RecCelebCard.jsx
@@ -1,5 +1,53 @@
+import styled, { css } from "styled-components";
+import { isMobile } from "react-device-detect";
+
 import TestAccountIcon from "@/assets/icon/TestAccountIcon.jsx";
 import FollowButton from "./FollowButton.jsx";
+
+const Styled = {
+  Container: styled.div`
+    background: ${({ theme }) => theme.color.white};
+    padding: 0.5rem 1.5rem;
+    height: 4.15rem;
+    display: flex;
+    align-items: center;
+    /* justify-content: space-between; */
+
+    border-radius: 0.25rem;
+    cursor: pointer;
+
+    ${({ $isMobile }) =>
+      $isMobile ||
+      css`
+        &:hover {
+          transform: ${({ theme }) => theme.transform.gridCard};
+          box-shadow: ${({ theme }) => theme.boxShadow.gridCard};
+          transition: ${({ theme }) => theme.transition.gridCard};
+        }
+      `}
+  `,
+  ProfileImage: styled.img`
+    width: 50px;
+    height: 50px;
+    border-radius: 9999px;
+  `,
+  TextContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 0.75rem;
+  `,
+  Text: styled.span`
+    &.celebName {
+      font-size: 14px;
+      margin-bottom: 0.25rem;
+    }
+    &.followerNum {
+      font-size: 12px;
+      color: ${({ theme }) => theme.color.addition};
+    }
+  `,
+};
+
 function RecCelebCard({
   celebId,
   celebName,
@@ -8,22 +56,27 @@ function RecCelebCard({
   isFollowing,
 }) {
   return (
-    <div>
+    <Styled.Container>
       {profileUrl ? (
-        <img src={profileUrl} alt={`${celebName} 프로필 사진`} />
+        <Styled.ProfileImage
+          src={profileUrl}
+          alt={`${celebName} 프로필 사진`}
+        />
       ) : (
         <TestAccountIcon size={"100"} />
       )}
-      <div>
-        <span>{celebName}</span>
-        <span>{followerNum || 0}명이 팔로우 중</span>
-      </div>
+      <Styled.TextContainer>
+        <Styled.Text className="celebName">{celebName}</Styled.Text>
+        <Styled.Text className="followerNum">
+          {followerNum || 0}명이 팔로우 중
+        </Styled.Text>
+      </Styled.TextContainer>
       <FollowButton
         celebId={celebId}
         isFollowing={isFollowing}
-        style={{ padding: "6px 8px", fontSize: "14px" }}
+        style={{ padding: "6px 8px", fontSize: "14px", marginLeft: "auto" }}
       />
-    </div>
+    </Styled.Container>
   );
 }
 
