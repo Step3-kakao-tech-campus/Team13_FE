b58cd4e72af08b6816b8f6b29f8fa5a44495ace0
Klomachenko <leekyumin0901@naver.com>
2023-10-13 15:08:57 +0900

    fix: PropTypes 변경

--- src/src/components/common/button/FollowButton.jsx
+++ src/src/components/common/button/FollowButton.jsx
@@ -4,15 +4,15 @@ import Button from "./Button.jsx";
 
 /**
  * 팔로우 버튼 컴포넌트
- * @param {node} children
+ * @param {React.ReactNode} children
  * @param {string} styleType 버튼 스타일 타입 BUTTON_TYPE.[PRIMARY || SECONDARY || TERTIARY]
  * @param {boolean} isHoverStyle hover 스타일링 적용 여부
- * @param {number} celebId 셀럽 아이디
+ * @param {string | number} celebId 셀럽 아이디
  * @param {boolean} isFollowing 팔로잉 여부
- * @param props 기타
+ * @param {html.Attributes} htmlButtonProps 기타
  */
 
-function FollowButton({ children, styleType, isHoverStyle, celebId, isFollowing, ...props }) {
+function FollowButton({ children, isHoverStyle , celebId, isFollowing, ...props }) {
     
   const handleFollowClick = () => {
     // 팔로우 버튼 클릭 시 로직
@@ -28,6 +28,7 @@ function FollowButton({ children, styleType, isHoverStyle, celebId, isFollowing,
         <Button
         styleType={BUTTON_TYPE.PRIMARY}
         onClick={handleUnFollowClick}
+        isHoverStyle={false}
         {...props}
       >
         팔로잉
@@ -36,6 +37,7 @@ function FollowButton({ children, styleType, isHoverStyle, celebId, isFollowing,
         <Button
           styleType={BUTTON_TYPE.SECONDARY}
           onClick={handleFollowClick}
+          isHoverStyle={false}
           {...props}
         >
           팔로우
