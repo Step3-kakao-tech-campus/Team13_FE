55f14a7167ab91bb01ddcefa0e8696032e26c812
j8won <joowoni99@gmail.com>
2023-10-14 18:02:55 +0900

    chore: prettier 파일에 따른 포매팅 변경

--- src/src/pages/CreateFund.page.jsx
+++ src/src/pages/CreateFund.page.jsx
@@ -109,7 +109,7 @@ function CreateFundPage() {
         <Styled.TitleInput
           placeholder="펀딩 제목을 입력하세요"
           value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={e => setTitle(e.target.value)}
         />
       </Styled.InputContainer>
 
