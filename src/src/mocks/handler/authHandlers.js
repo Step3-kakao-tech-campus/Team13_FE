1f21d3cc9add6957ca2e48e9cc3dace23af60f89
j8won <joowoni99@gmail.com>
2023-10-15 17:02:30 +0900

    feat: mock authHandlers post login response handler 수정

--- src/src/mocks/handler/authHandlers.js
+++ src/src/mocks/handler/authHandlers.js
@@ -3,9 +3,13 @@ import API from "@/constants/API.js";
 
 export const authHandlers = [
   rest.post("/api" + API.AUTH.LOGIN, (req, res, ctx) => {
-    const { email, password } = req.json();
+    const { email, password } = req.body;
 
-    if (!email || !password) return res(ctx.status(400));
+    if (!email || !password)
+      return res(
+        ctx.status(400),
+        ctx.json({ email: email, password: password }),
+      );
 
     return res(
       ctx.status(200),
