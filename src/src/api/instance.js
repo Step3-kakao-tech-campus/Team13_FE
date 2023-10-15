a905dce38b6ebb121151814819bb76d4647ad131
j8won <joowoni99@gmail.com>
2023-10-15 14:36:09 +0900

    feat: instance에 request interceptors 추가

--- src/src/api/instance.js
+++ src/src/api/instance.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import TOKEN from "@/constants/TOKEN.js";
 
 const baseUrl = import.meta.env.VITE_USE_MOCK_API
   ? `${import.meta.env.BASE_URL}/api`
@@ -10,5 +11,19 @@ const instance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+instance.interceptors.request.use(
+  function (config) {
+    const accessToken = localStorage.getItem(TOKEN.ACCESS);
+
+    if (!accessToken) return config;
+
+    config.headers.Authorization = accessToken;
+    return config;
+  },
+  function (err) {
+    return Promise.reject(err);
+  },
+);
 
 export default instance;
