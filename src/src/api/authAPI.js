25385c00145b98c74530b1d83cd640bc323db839
j8won <joowoni99@gmail.com>
2023-10-15 22:06:22 +0900

    docs: authAPI.js에 jsdoc 추가

--- src/src/api/authAPI.js
+++ src/src/api/authAPI.js
@@ -1,6 +1,12 @@
 import instance from "@/api/instance.js";
 import API from "@/constants/API.js";
 
+/**
+ * 로그인 요청 api
+ * @param {string} email
+ * @param {string} password
+ * @returns {Promise<axios.AxiosResponse<any>>}
+ */
 const postLogin = async ({ email, password }) => {
   return await instance({
     url: API.AUTH.LOGIN,
@@ -9,6 +15,12 @@ const postLogin = async ({ email, password }) => {
   });
 };
 
+/**
+ * accesstoken 재발급 api
+ * @param {string} baseUrl
+ * @param {string} refreshToken
+ * @returns {string}
+ */
 const refreshToken = ({ baseUrl, refreshToken }) => {
   // api 통신
   return "refreshedAccessToken";
