14b93be7f6daa659d823ecc795a1784915f992f9
j8won <joowoni99@gmail.com>
2023-10-15 15:16:26 +0900

    chore: Login.page.jsx 코드 포매팅 변경

--- src/src/pages/Login.page.jsx
+++ src/src/pages/Login.page.jsx
@@ -39,8 +39,8 @@ function LoginPage() {
       <FormTemplate>
         <Styled.LoginTitle>로그인</Styled.LoginTitle>
         <Form
-          onSubmit={(data) => console.log(data)}
-          onError={(err) => console.log(err)}
+          onSubmit={data => console.log(data)}
+          onError={err => console.log(err)}
           inputInformations={FORM_INFO.SIGN_IN}
           defaultValues={FORM_DEFAULT.SIGN_IN}
         >
