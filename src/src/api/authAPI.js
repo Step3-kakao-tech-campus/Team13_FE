480e3ceba00d89398766e840cfb8fea30133f4e7
j8won <joowoni99@gmail.com>
2023-10-15 14:55:28 +0900

    feat: instance에 response interceptors 추가

---
+++ src/src/api/authAPI.js
@@ -0,0 +1,6 @@
+const refreshToken = ({ baseUrl, refreshToken }) => {
+  // api 통신
+  return "refreshedAccessToken";
+};
+
+export default { refreshToken };
--- src/src/api/instance.js
+++ src/src/api/instance.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import TOKEN from "@/constants/TOKEN.js";
+import authAPI from "@/api/authAPI.js";
 
 const baseUrl = import.meta.env.VITE_USE_MOCK_API
   ? `${import.meta.env.BASE_URL}/api`
@@ -18,11 +19,40 @@ instance.interceptors.request.use(
 
     if (!accessToken) return config;
 
-    config.headers.Authorization = accessToken;
+    config.headers.Authorization = `Bearer accessToken`;
     return config;
   },
   function (err) {
     return Promise.reject(err);
+  },
+);
+
+instance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (err) {
+    if (!err.response || !err.response.status) {
+      return Promise.reject(err);
+    }
+
+    switch (err.response.status) {
+      case 401: {
+        localStorage.removeItem(TOKEN.ACCESS);
+        const originalRequest = err.config;
+        const refreshToken = localStorage.getItem(TOKEN.REFRESH);
+
+        if (!refreshToken) return Promise.reject(err);
+
+        const newAccessToken = authAPI.refreshToken({ baseUrl, refreshToken });
+        localStorage.setItem(TOKEN.ACCESS, newAccessToken);
+
+        originalRequest.headers.authorization = `Bearer ${newAccessToken}`;
+        return axios(originalRequest);
+      }
+      default:
+        return Promise.reject(err);
+    }
   },
 );
 
