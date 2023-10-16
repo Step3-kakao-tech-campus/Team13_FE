f04c36d814275ec8d3d39551aabee3fc0104789a
sihyonn <sihyeon982@naver.com>
2023-10-17 00:31:06 +0900

    feat: 1,2,3 순위 아이콘 추가

---
+++ src/src/assets/icon/SecondRibbonIcon.jsx
@@ -0,0 +1,20 @@
+function SecondRibbonIcon() {
+  return (
+    <>
+      <img
+        width="48"
+        height="48"
+        src="https://img.icons8.com/color/48/second-place-ribbon.png"
+        alt="second-place-ribbon"
+      />
+      <div id="license" style={{ display: "none" }}>
+        <a href="https://icons8.com/icon/2AxMUk3BMPIc/second-place-ribbon">
+          Second Place Ribbon
+        </a>
+        icon by <a href="https://icons8.com">Icons8</a>
+      </div>
+    </>
+  );
+}
+
+export default SecondRibbonIcon;
---
+++ src/src/assets/icon/ThirdRibbonIcon.jsx
@@ -0,0 +1,20 @@
+function ThirdRibbonIcon() {
+  return (
+    <>
+      <img
+        width="48"
+        height="48"
+        src="https://img.icons8.com/color/48/third-place-ribbon.png"
+        alt="third-place-ribbon"
+      />
+      <div id="license" style={{ display: "none" }}>
+        <a href="https://icons8.com/icon/2WK1cEIaoUzY/third-place-ribbon">
+          Third Place Ribbon
+        </a>
+        icon by <a href="https://icons8.com">Icons8</a>
+      </div>
+    </>
+  );
+}
+
+export default ThirdRibbonIcon;
