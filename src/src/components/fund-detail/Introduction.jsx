6f03c52c70789c9fd07a01b749f94b529ee4f4fa
j8won <joowoni99@gmail.com>
2023-10-18 15:06:14 +0900

    feat: 펀딩 상세 페이지 소개 탭의 공동관리자 컴포넌트 CoAdministrator.jsx 생성

--- src/src/components/fund-detail/Introduction.jsx
+++ src/src/components/fund-detail/Introduction.jsx
@@ -1,5 +1,54 @@
+import styled from "styled-components";
+import CoAdministrator from "@/components/fund-detail/CoAdministrator.jsx";
+
+const Styled = {
+  Container: styled.div``,
+  Title: styled.div`
+    font-size: 1.5rem;
+    font-weight: 600;
+  `,
+  CoAdministratorBox: styled.div`
+    padding: 1rem 0 2rem 0;
+    display: grid;
+    grid-template-columns: repeat(3, 200px);
+    grid-gap: 1rem;
+
+    @media screen and (max-width: calc(600px + 2rem + 4rem)) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media screen and (max-width: calc(400px + 1rem + 4rem)) {
+      grid-template-columns: repeat(1, 100%);
+    }
+  `,
+};
+
+const coAdminInfo = {
+  userId: "sonnyfan",
+  profileUrl:
+    "https://velog.velcdn.com/images/j8won/profile/55917697-2140-40be-ad07-d2d02137f38e/image.jpeg",
+  nickname: "마이쏘니",
+};
+
 function Introduction() {
-  return <div>소개글</div>;
+  return (
+    <Styled.Container>
+      <Styled.Title>공동관리자</Styled.Title>
+      <Styled.CoAdministratorBox>
+        {Array(3)
+          .fill(coAdminInfo)
+          .map((coAdmin, index) => (
+            <CoAdministrator
+              key={index}
+              userId={coAdmin.userId}
+              profileUrl={coAdmin.profileUrl}
+              nickname={coAdmin.nickname}
+            />
+          ))}
+      </Styled.CoAdministratorBox>
+      <Styled.Title>소개글</Styled.Title>
+    </Styled.Container>
+  );
 }
 
 export default Introduction;
