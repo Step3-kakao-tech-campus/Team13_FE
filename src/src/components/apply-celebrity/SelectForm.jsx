e43d6bf4d51a082daa18470a56be76230fb704b5
sihyonn <sihyeon982@naver.com>
2023-10-19 14:35:58 +0900

    style: SelectForm 스타일링 적용

--- src/src/components/apply-celebrity/SelectForm.jsx
+++ src/src/components/apply-celebrity/SelectForm.jsx
@@ -1,4 +1,23 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const Styled = {
+  SelectContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
+  Label: styled.label`
+    padding-bottom: 0.5rem;
+    font-size: 1rem;
+  `,
+  Select: styled.select`
+    padding: 0.75rem 1rem;
+    margin-bottom: 1rem;
+    width: 100%;
+    height: 3rem;
+    font-size: 1rem;
+  `,
+};
 
 function SelectForm({ options, label, onChange }) {
   const [selectedOption, setSelectedOption] = useState("");
@@ -10,17 +29,21 @@ function SelectForm({ options, label, onChange }) {
   };
 
   return (
-    <div>
-      <label htmlFor="select">{label}</label>
-      <select id="select" value={selectedOption} onChange={handleSelectChange}>
+    <Styled.SelectContainer>
+      <Styled.Label htmlFor="select">{label}</Styled.Label>
+      <Styled.Select
+        id="select"
+        value={selectedOption}
+        onChange={handleSelectChange}
+      >
         <option value="">선택하세요</option>
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
           </option>
         ))}
-      </select>
-    </div>
+      </Styled.Select>
+    </Styled.SelectContainer>
   );
 }
 
