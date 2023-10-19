52107a98d7fd048141926af8313a8ed0c2528813
sihyonn <sihyeon982@naver.com>
2023-10-19 14:35:16 +0900

    style: 셀럽신청모달 스타일링 적용

--- src/src/components/apply-celebrity/CelebApplyModal.jsx
+++ src/src/components/apply-celebrity/CelebApplyModal.jsx
@@ -1,8 +1,32 @@
 import { useState } from "react";
+import styled from "styled-components";
+
 import BackdropModal from "../common/modal/BackdropModal.jsx";
 import ThumbnailBox from "../create-fund/ThumbnailBox.jsx";
 import Button from "../common/button/Button.jsx";
 import SelectForm from "./SelectForm.jsx";
+
+const Styled = {
+  Title: styled.div`
+    font-size: 1.5rem;
+    font-weight: 600;
+  `,
+  FormContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
+  Label: styled.label`
+    padding-bottom: 0.5rem;
+    font-size: 1rem;
+  `,
+  Input: styled.input`
+    padding: 0.75rem 1rem;
+    margin-bottom: 1rem;
+    width: 100%;
+    height: 3rem;
+    font-size: 1rem;
+  `,
+};
 
 function CelebApplyModal({ setOpen }) {
   const [thumbnailFile, setThumbnailFile] = useState(null);
@@ -34,15 +58,18 @@ function CelebApplyModal({ setOpen }) {
       modalStyle={{
         width: "22.4rem",
         height: "43.5rem",
-        padding: "2rem 3.4rem",
+        padding: "2.5rem 3.4rem",
       }}
     >
-      <h1>셀럽 신청</h1>
+      <Styled.Title>셀럽 신청</Styled.Title>
       <ThumbnailBox file={thumbnailFile} setFile={setThumbnailFile} />
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <labe>이름</labe>
-        <input type="text"></input>
 
+      <Styled.FormContainer>
+        <Styled.Label>이름</Styled.Label>
+        <Styled.Input
+          type="text"
+          placeholder="이름을 입력해주세요"
+        ></Styled.Input>
         <SelectForm
           options={genderOptions}
           label="성별"
@@ -53,15 +80,15 @@ function CelebApplyModal({ setOpen }) {
           label="분류"
           onChange={handleCategoryChange}
         />
-
-        <labe>소속그룹</labe>
-        <input type="text" placeholder="선택사항"></input>
-      </div>
+        <Styled.Label>소속그룹</Styled.Label>
+        <Styled.Input type="text" placeholder="선택사항"></Styled.Input>
+      </Styled.FormContainer>
 
       <Button
         style={{
           padding: "0.75rem",
           width: "100%",
+          maringTop: "0.5rem",
         }}
       >
         신청하기
