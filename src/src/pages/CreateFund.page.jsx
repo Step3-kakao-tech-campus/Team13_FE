efc61604eaa1fcdea6d37458336eb8026c1626a2
j8won <joowoni99@gmail.com>
2023-10-22 00:23:31 +0900

    refactor: CreateFund.page - Styled InputContainer, TitleInput을 CommonStyle.js로 분리

--- src/src/pages/CreateFund.page.jsx
+++ src/src/pages/CreateFund.page.jsx
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 import { useState } from "react";
-import TextareaAutoSize from "react-textarea-autosize";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
-import { Title } from "@/styles/CommonStyle";
+import {
+  Title,
+  FundTitleInput,
+  WhiteInputContainer,
+} from "@/styles/CommonStyle";
 import ThumbnailBox from "@/components/create-fund/ThumbnailBox.jsx";
 import SettingForm from "@/components/create-fund/SettingForm.jsx";
 import formatDateToYYYYMMDD from "@/utils/formateDateToYYYYMMDD.js";
@@ -20,41 +23,6 @@ const Styled = {
     #thumbnail-note {
       font-size: 0.75rem;
       color: ${({ theme }) => theme.color.alertBlue};
-    }
-  `,
-  InputContainer: styled.article`
-    margin: 2rem 0;
-
-    padding: 2rem;
-    width: 100%;
-    background-color: ${({ theme }) => theme.color.white};
-    border: ${({ theme }) => theme.border.main};
-    border-radius: 0.25rem;
-  `,
-  TitleInput: styled(TextareaAutoSize)`
-    padding: 0 0.5rem 0.5rem;
-    width: 100%;
-    resize: none;
-
-    font-size: 1.5rem;
-    color: ${({ theme }) => theme.color.body};
-
-    overflow-wrap: break-word;
-    word-break: break-all;
-    white-space: pre-wrap;
-
-    border: none;
-    border-bottom: 2px solid ${({ theme }) => theme.color.inactive};
-    outline: none;
-
-    transition: border-bottom-color ease-in-out 0.3s;
-
-    &::placeholder {
-      color: ${({ theme }) => theme.color.addition};
-    }
-
-    &:focus {
-      border-bottom: 2px solid ${({ theme }) => theme.color.mainRed};
     }
   `,
   Subtitle: styled.div`
@@ -105,13 +73,13 @@ function CreateFundPage() {
   return (
     <Styled.Container>
       <Title>펀딩 주최하기</Title>
-      <Styled.InputContainer>
-        <Styled.TitleInput
+      <WhiteInputContainer>
+        <FundTitleInput
           placeholder="펀딩 제목을 입력하세요"
           value={title}
-          onChange={e => setTitle(e.target.value)}
+          onChange={(e) => setTitle(e.target.value)}
         />
-      </Styled.InputContainer>
+      </WhiteInputContainer>
 
       <Styled.Subtitle>썸네일</Styled.Subtitle>
       <div id="thumbnail-note" style={{ fontSize: "0.75rem" }}>
@@ -120,9 +88,9 @@ function CreateFundPage() {
       <ThumbnailBox file={thumbnailFile} setFile={setThumbnailFile} />
 
       <Styled.Subtitle>펀딩 설정</Styled.Subtitle>
-      <Styled.InputContainer>
+      <WhiteInputContainer>
         <SettingForm input={settingInput} setInput={setSettingInput} />
-      </Styled.InputContainer>
+      </WhiteInputContainer>
 
       <Styled.Subtitle>펀딩 소개</Styled.Subtitle>
       <TextEditor
