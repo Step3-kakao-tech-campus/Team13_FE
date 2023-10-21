5cedd3e9e7cdb852d2d6c075f1780bf25a099eca
j8won <joowoni99@gmail.com>
2023-10-22 00:25:53 +0900

    feat: FundTextEdit.page.jsx api 통신을 제외한 로직과 UI

--- src/src/constants/EDIT_TYPE.js
+++ src/src/constants/EDIT_TYPE.js
@@ -3,12 +3,24 @@ const EDIT_TYPE = {
   FUND_UPDATE: "fundUpdate",
 };
 
-const TITLE = {
+const PAGE_TITLE = {
   fundIntroduction: "펀딩 소개 수정",
   fundUpdate: "펀딩 업데이트 작성",
 };
 
+const TITLE = {
+  fundIntroduction: "펀딩 소개 수정하기",
+  fundUpdate: "펀딩 업데이트 작성하기",
+};
+
+const BUTTON = {
+  fundIntroduction: "수정하기",
+  fundUpdate: "업로드",
+};
+
 Object.freeze(EDIT_TYPE);
 Object.freeze(TITLE);
+Object.freeze(TITLE);
+Object.freeze(BUTTON);
 
-export default { ...EDIT_TYPE, TITLE };
+export default { ...EDIT_TYPE, PAGE_TITLE, TITLE, BUTTON };
