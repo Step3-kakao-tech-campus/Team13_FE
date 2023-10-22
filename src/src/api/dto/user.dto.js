fc981a6b766dbea04faac3cac402279d9f660471
j8won <joowoni99@gmail.com>
2023-10-22 20:45:29 +0900

    feat: UserSettingDto에 profileImageFile 추가

--- src/src/api/dto/user.dto.js
+++ src/src/api/dto/user.dto.js
@@ -1,8 +1,16 @@
 class UserSettingDto {
-  constructor({ nickname, phoneNumber, profileUrl, password, newPassword }) {
+  constructor({
+    nickname,
+    phoneNumber,
+    profileUrl,
+    profileFile,
+    password,
+    newPassword,
+  }) {
     this.nickname = nickname;
     this.phoneNumber = phoneNumber;
     this.profileImageUrl = profileUrl;
+    this.profileImageFile = profileFile;
     this.password = password || "";
     this.newPassword = newPassword || "";
   }
