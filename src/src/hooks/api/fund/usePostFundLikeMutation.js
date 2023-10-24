b78f71315f69d305c0e205cc3b62e9857191df28
j8won <joowoni99@gmail.com>
2023-10-24 16:27:26 +0900

    feat: 펀딩 좋아요 추가, 취소 커스텀 훅 생성

---
+++ src/src/hooks/api/fund/usePostFundLikeMutation.js
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import API from "@/constants/API.js";
+import fundAPI from "@/api/fundAPI.js";
+import toast from "react-hot-toast";
+
+function usePostFundLikeMutation(handleSuccess) {
+  return useMutation(
+    [API.FUND.LIKE],
+    async ({ fundId }) => {
+      return fundAPI.postFundLike(fundId);
+    },
+    {
+      onError: (err) => {
+        toast.error(err.message);
+      },
+      onSuccess: handleSuccess,
+    },
+  );
+}
+
+export default usePostFundLikeMutation;
