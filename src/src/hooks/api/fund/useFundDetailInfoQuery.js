c1994bcfbaf3963b53dfb331ffa73b3e0b27d260
j8won <joowoni99@gmail.com>
2023-10-26 17:05:07 +0900

    feat: 펀딩 상세 정보 조회 api 커스텀 훅 생성

---
+++ src/src/hooks/api/fund/useFundDetailInfoQuery.js
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import API from "@/constants/API.js";
+import FundAPI from "@/api/fundAPI.js";
+
+function useFundDetailInfoQuery({ fundId }) {
+  return useQuery(
+    [API.FUND.DETAIL(fundId)],
+    async () => {
+      return await FundAPI.getDetailInfoByFundId(fundId);
+    },
+    {},
+  );
+}
+
+export default useFundDetailInfoQuery;
