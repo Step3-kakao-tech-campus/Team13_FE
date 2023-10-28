326e442de71124f5964e59585eb4294ffee57c66
sihyonn <sihyeon982@naver.com>
2023-10-28 16:05:00 +0900

    feat: CelebRank - 스타일링, PropTypes, jsdoc추가

--- src/src/components/celebrity-detail/CelebRank.jsx
+++ src/src/components/celebrity-detail/CelebRank.jsx
@@ -1,6 +1,37 @@
+import { styled } from "styled-components";
+import { PropTypes } from "prop-types";
+
 import FirstRibbonIcon from "@/assets/icon/FirstRibbonIcon.jsx";
 import SecondRibbonIcon from "@/assets/icon/SecondRibbonIcon.jsx";
 import ThirdRibbonIcon from "@/assets/icon/ThirdRibbonIcon.jsx";
+
+const Styled = {
+  RankContainer: styled.div`
+    display: flex;
+  `,
+  Rank: styled.div`
+    margin: 0 0.7rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    & div {
+      margin-bottom: 0.5rem;
+      font-size: 1.5rem;
+      font-weight: 500;
+    }
+
+    & span {
+      color: ${({ theme }) => theme.color.addition};
+    }
+  `,
+};
+
+/**
+ * 셀럽순위 표시 컴포넌트 - 팔로워수, 총펀딩금액순으로 순위 표기. 1,2,3 등은 아이콘 그 외 텍스트 등수표시
+ * @param {string} props.followerRank - 팔로워 순위
+ * @param {string} props.fundingRank- 총 펀딩금액 순위
+ */
 
 function CelebRank({ followerRank, fundingRank }) {
   const renderIcon = (rank) => {
@@ -13,17 +44,22 @@ function CelebRank({ followerRank, fundingRank }) {
   };
 
   return (
-    <div className="순위">
-      <div className="팔로워순위">
+    <Styled.RankContainer>
+      <Styled.Rank>
         <div>{renderIcon(followerRank)}</div>
-        <div>팔로워</div>
-      </div>
-      <div className="펀딩금액순위">
+        <span>팔로워</span>
+      </Styled.Rank>
+      <Styled.Rank>
         <div>{renderIcon(fundingRank)}</div>
-        <div>펀딩금액</div>
-      </div>
-    </div>
+        <span>펀딩금액</span>
+      </Styled.Rank>
+    </Styled.RankContainer>
   );
 }
+
+CelebRank.propTypes = {
+  followerRank: PropTypes.number.isRequired,
+  fundingRank: PropTypes.number.isRequired,
+};
 
 export default CelebRank;
