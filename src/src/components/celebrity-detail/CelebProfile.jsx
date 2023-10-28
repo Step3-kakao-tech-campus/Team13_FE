279a68693d4e428ff465e9a6cf72ff260fe1aabb
sihyonn <sihyeon982@naver.com>
2023-10-28 14:42:34 +0900

    feat: CelebProfile 스타일링, PropTypes추가

--- src/src/components/celebrity-detail/CelebProfile.jsx
+++ src/src/components/celebrity-detail/CelebProfile.jsx
@@ -1,3 +1,36 @@
+import styled from "styled-components";
+import { PropTypes } from "prop-types";
+
+const Styled = {
+  ProfileContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
+
+  NameAndAffiliation: styled.div`
+    font-size: 1.5rem;
+    font-weight: 600;
+
+    & span {
+      font-size: 1rem;
+      font-weight: 400;
+      margin-left: 0.5rem;
+      color: ${({ theme }) => theme.color.addition};
+    }
+  `,
+
+  CategoryAndGender: styled.div`
+    margin: 0.5rem 0;
+    font-size: 1rem;
+    color: ${({ theme }) => theme.color.addition};
+  `,
+
+  Img: styled.img`
+    width: 9rem;
+    border-radius: 0.25rem;
+  `,
+};
+
 function CelebProfile({
   celebName,
   affiliation,
@@ -6,17 +39,25 @@ function CelebProfile({
   profileUrl,
 }) {
   return (
-    <div>
-      <div className="nameAndAffiliation">
-        <span>{celebName}</span>
+    <Styled.ProfileContainer>
+      <Styled.NameAndAffiliation>
+        {celebName}
         {affiliation && <span>{affiliation}</span>}
-      </div>
-      <div className="categoryAndGender">
-        <span>{`${celebCategory} • ${celebGender}`}</span>
-      </div>
-      <img src={profileUrl} alt="셀럽프로필사진" style={{ width: "9rem" }} />
-    </div>
+      </Styled.NameAndAffiliation>
+
+      <Styled.CategoryAndGender>{`${celebCategory} • ${celebGender}`}</Styled.CategoryAndGender>
+
+      <Styled.Img src={profileUrl} alt="셀럽프로필사진" />
+    </Styled.ProfileContainer>
   );
 }
 
 export default CelebProfile;
+
+CelebProfile.PropTypes = {
+  celebName: PropTypes.string.isRequired,
+  affiliation: PropTypes.string,
+  celebCategory: PropTypes.string.isRequired,
+  celebGender: PropTypes.string.isRequired,
+  profileUrl: PropTypes.string,
+};
