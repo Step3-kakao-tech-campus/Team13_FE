a26a10a0358b9a93963adb4d9bf7d743df0e74c8
j8won <joowoni99@gmail.com>
2023-10-31 00:13:03 +0900

    refactor: useSetImageFileToUrl 매개변수 변경

--- src/src/hooks/useSetImageFileToUrl.js
+++ src/src/hooks/useSetImageFileToUrl.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
+import { PropTypes } from "prop-types";
 
 /**
  * 파일, 이미지 url, 파일 변경 핸들러, 파일 삭제 핸들러를 반환하는 커스텀 훅
+ * @param {string=} initialImageUrl 초기 이미지 url
  * @returns {{ file: blob, imageUrl: string, handleFileChange: function, handleFileDelete: function}}
  */
 
-function useSetImageFileToUrl({ initialImage }) {
+function useSetImageFileToUrl(initialImageUrl) {
   const [file, setFile] = useState(null);
   const [imageUrl, setImageUrl] = useState("");
 
@@ -25,15 +27,16 @@ function useSetImageFileToUrl({ initialImage }) {
     };
 
     async function fetchData() {
-      setImageUrl(initialImage);
-      const newFile = await convertURLtoFile(initialImage);
+      setImageUrl(initialImageUrl);
+      const newFile = await convertURLtoFile(initialImageUrl);
       if (newFile) {
         setFile(newFile);
       }
     }
 
+    if (!initialImageUrl) return;
     fetchData();
-  }, [initialImage]);
+  }, [initialImageUrl]);
 
   useEffect(() => {
     if (!file) {
@@ -58,5 +61,9 @@ function useSetImageFileToUrl({ initialImage }) {
 
   return { file, imageUrl, handleFileChange, handleFileDelete };
 }
+
+useSetImageFileToUrl.propTypes = {
+  initialImage: PropTypes.string,
+};
 
 export default useSetImageFileToUrl;
