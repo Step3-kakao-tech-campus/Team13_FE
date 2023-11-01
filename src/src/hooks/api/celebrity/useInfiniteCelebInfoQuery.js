2d589dcf3a37676608fecee2ffd2ff6388e9a8a4
sihyonn <sihyeon982@naver.com>
2023-11-02 00:20:19 +0900

    feat: useInfiniteCelebInfoQuery.js 생성

---
+++ src/src/hooks/api/celebrity/useInfiniteCelebInfoQuery.js
@@ -0,0 +1,24 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import API from "@/constants/API.js";
+import celebrityAPI from "@/api/celebrityAPI.js";
+
+function useInfiniteCelebInfoQuery({ keyword, sortType }) {
+  return useInfiniteQuery(
+    [API.FUND.LIST, keyword, sortType],
+    async ({ pageParam = 0 }) => {
+      return celebrityAPI.getCelebInfoList({
+        pageIndex: pageParam,
+        keyword: keyword,
+        sortType: sortType,
+      });
+    },
+    {
+      suspense: true,
+      getNextPageParam: (lastPage) => {
+        return lastPage.config.params.pageIndex + 1;
+      },
+    },
+  );
+}
+
+export default useInfiniteCelebInfoQuery;
