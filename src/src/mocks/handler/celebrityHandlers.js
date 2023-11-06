38be6a659a88759c14dc44f50145139140b1bf8d
sihyonn <sihyeon982@naver.com>
2023-11-06 19:43:43 +0900

    feat: 셀럽관련 펀딩목록 조회 핸들러 및 데이터 추가

--- src/src/mocks/handler/celebrityHandlers.js
+++ src/src/mocks/handler/celebrityHandlers.js
@@ -120,6 +120,45 @@ const youngCelebDetailInfo = {
   },
 };
 
+//셀럽관련 펀딩목록 조회 데이터
+const sonnyFundInfo1 = {
+  celebId: 1,
+  fundId: 1,
+  fundTitle:
+    "손흥민 주장된 기념 지하철 광고 🎉🎉 축구중독자가 책임지고 펀딩합니다 ❤️‍🔥",
+  thumbnailUrl:
+    "https://ichef.bbci.co.uk/news/640/cpsprodpb/4118/production/_119546661_gettyimages-1294130887.jpg",
+  targetDate: "2023-12-17",
+  targetMoney: "3000000",
+  currentMoney: "2340000",
+  celebrityId: "sonny",
+  celebrityName: "손흥민",
+  celebrityProfileUrl:
+    "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202308/13/3756de8c-1ea6-4988-b063-25f26d9b76d5.jpg",
+  organizerId: "soccer123",
+  organizerName: "축구도사",
+  isInUserWishList: true,
+};
+
+const sonnyFundInfo2 = {
+  celebId: 1,
+  fundId: 2,
+  fundTitle: "쏘니 폼 미쳤다 토트넘역 지하철 광고판 달자",
+  thumbnailUrl:
+    "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltaf10a2ea551a3e54/6360dc8f67675010b765f257/GettyImages-1432946487.jpg",
+  targetDate: "2023-12-24",
+  targetMoney: "5000000",
+  currentMoney: "100000",
+  celebrityId: "sonny",
+  celebrityName: "손흥민",
+  celebrityProfileUrl:
+    "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202308/13/3756de8c-1ea6-4988-b063-25f26d9b76d5.jpg",
+  isFollowing: false,
+  organizerId: "soccer234",
+  organizerName: "싸커이삼사",
+  isInUserWishList: false,
+};
+
 export const celebrityHandlers = [
   // 셀럽 목록 조회
   rest.get("/api" + API.CELEBRITY.LIST, (req, res, ctx) => {
@@ -224,5 +263,29 @@ export const celebrityHandlers = [
         ctx.json({ message: "존재하지 않는 셀럽입니다" }),
       );
     }
+  }),
+
+  // 셀럽관련 펀딩목록 조회
+  rest.get("/api" + API.CELEBRITY.FUNDING(":celebId"), (req, res, ctx) => {
+    const keyword = req.url.searchParams.get("keyword");
+    const pageIndex = req.url.searchParams.get("pageIndex");
+    const sortType = req.url.searchParams.get("sortType");
+    const { celebId } = req.params;
+
+    if (!pageIndex) return res(ctx.status(400, "pageIndex 없음"));
+    if (!celebId) return res(ctx.status(400, "celebId 없음"));
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        celebRelatedFundList: Array.from({ length: 12 }, (_, i) =>
+          i % 2 ? sonnyFundInfo2 : sonnyFundInfo1,
+        ),
+        isLastPage: false,
+        currentPage: pageIndex,
+        keyword: keyword,
+        sortType: sortType,
+      }),
+    );
   }),
 ];
