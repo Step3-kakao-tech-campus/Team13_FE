89c0fa85202ed2077f22de24af07ea6493ad6828
sihyonn <sihyeon982@naver.com>
2023-11-09 17:25:06 +0900

    feat: 마이펀딩 팔로잉한 셀럽조회 api, SimpleCelebInfo dto 추가

--- src/src/api/myFundAPI.js
+++ src/src/api/myFundAPI.js
@@ -1,6 +1,7 @@
 import instance from "@/api/instance.js";
 import API from "@/constants/API.js";
 import { MyFundUserInfoDto } from "./dto/myFund.dto.js";
+import { SimpleCelebInfoDto } from "./dto/celebrity.dto.js";
 
 /**
  * My펀딩 사용자 정보 조회 api
@@ -14,4 +15,16 @@ const getMyFundUserInfoByToken = async () => {
   return new MyFundUserInfoDto(data);
 };
 
-export default { getMyFundUserInfoByToken };
+/**
+ * My펀딩 팔로잉한 셀럽 조회 api
+ */
+
+const getFollowingCelebByToken = async () => {
+  const { data } = await instance({
+    url: API.MY_FUND.FOLLOW,
+    method: "GET",
+  });
+  return new SimpleCelebInfoDto(data);
+};
+
+export default { getMyFundUserInfoByToken, getFollowingCelebByToken };
