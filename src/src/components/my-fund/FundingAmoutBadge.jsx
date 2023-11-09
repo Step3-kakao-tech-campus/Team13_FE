aa80fe3ed30dc98cd47ac82d38817d5c1e1ad3a7
sihyonn <sihyeon982@naver.com>
2023-11-09 15:02:45 +0900

    feat: 펀딩금액뱃지 proptypes, jsdoc추가

--- src/src/components/my-fund/FundingAmoutBadge.jsx
+++ src/src/components/my-fund/FundingAmoutBadge.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { PropTypes } from "prop-types";
 
 const Styled = {
   Container: styled.div`
@@ -14,12 +15,23 @@ const Styled = {
   `,
 };
 
-function FundingAmountBadge() {
+/**
+ * My펀딩페이지 - 펀딩내역 카드에 표시되는 내가 펀딩한 금액 뱃지
+ * @params {string | number} 펀딩한 금액
+ */
+
+function FundingAmountBadge({ fundingAmount = 500000 }) {
   return (
     <Styled.Container>
-      <Styled.Amount>10,000원</Styled.Amount>
+      <Styled.Amount>
+        {Number(fundingAmount).toLocaleString("ko-KR")}원
+      </Styled.Amount>
     </Styled.Container>
   );
 }
+
+FundingAmountBadge.PropTypes = {
+  fundingAmount: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+};
 
 export default FundingAmountBadge;
