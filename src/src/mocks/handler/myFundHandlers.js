2330f48988606fd0a136866b4c7c9aa84f3ebbc8
sihyonn <sihyeon982@naver.com>
2023-11-10 00:16:22 +0900

    feat: 후원한 펀딩목록 조회 handlers 추가, 데이터추가

--- src/src/mocks/handler/myFundHandlers.js
+++ src/src/mocks/handler/myFundHandlers.js
@@ -1,6 +1,44 @@
 import { rest } from "msw";
 import API from "@/constants/API.js";
 
+const sonnyFundInfo1 = {
+  fundId: 1,
+  fundTitle:
+    "손흥민 주장된 기념 지하철 광고 🎉🎉 축구중독자가 책임지고 펀딩합니다 ❤️‍🔥",
+  thumbnailUrl:
+    "https://ichef.bbci.co.uk/news/640/cpsprodpb/4118/production/_119546661_gettyimages-1294130887.jpg",
+  targetDate: "2023-12-17",
+  targetMoney: "3000000",
+  currentMoney: "2340000",
+  paymentAmount: "100000",
+  celebrityId: "sonny",
+  celebrityName: "손흥민",
+  celebrityProfileUrl:
+    "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202308/13/3756de8c-1ea6-4988-b063-25f26d9b76d5.jpg",
+  organizerId: "soccer123",
+  organizerName: "축구도사",
+  isInUserWishList: true,
+};
+
+const sonnyFundInfo2 = {
+  fundId: 2,
+  fundTitle: "쏘니 폼 미쳤다 토트넘역 지하철 광고판 달자",
+  thumbnailUrl:
+    "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltaf10a2ea551a3e54/6360dc8f67675010b765f257/GettyImages-1432946487.jpg",
+  targetDate: "2023-12-24",
+  targetMoney: "5000000",
+  currentMoney: "100000",
+  paymentAmount: "250000",
+  celebrityId: "sonny",
+  celebrityName: "손흥민",
+  celebrityProfileUrl:
+    "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202308/13/3756de8c-1ea6-4988-b063-25f26d9b76d5.jpg",
+  isFollowing: false,
+  organizerId: "soccer234",
+  organizerName: "싸커이삼사",
+  isInUserWishList: false,
+};
+
 export const myFundHandlers = [
   // 사용자 정보 조회
   rest.get("/api" + API.MY_FUND.NICKNAME, (req, res, ctx) => {
@@ -42,6 +80,24 @@ export const myFundHandlers = [
             followerNum: 6500,
           },
         ],
+      }),
+    );
+  }),
+
+  // 후원한 펀딩목록 조회
+  rest.get("/api" + API.MY_FUND.SUPPORT, (req, res, ctx) => {
+    const pageIndex = req.url.searchParams.get("pageIndex");
+
+    if (!pageIndex) return res(ctx.status(400, "pageIndex 없음"));
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        isLastPage: false,
+        currentPage: pageIndex,
+        fundList: Array.from({ length: 12 }, (_, i) =>
+          i % 2 ? sonnyFundInfo2 : sonnyFundInfo1,
+        ),
       }),
     );
   }),
