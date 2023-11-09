d5f404b7f3dd644b2ab34e5af24cfd9e84448ff5
sihyonn <sihyeon982@naver.com>
2023-11-09 18:14:45 +0900

    feat: 마이펀딩 팔로잉한 셀럽 조회 api 연결

--- src/src/components/my-fund/MyFollowingList.jsx
+++ src/src/components/my-fund/MyFollowingList.jsx
@@ -1,26 +1,19 @@
 import { GridTemplate } from "@/styles/CommonStyle";
 import SimpleCelebCard from "../celebrity/SimpleCelebCard.jsx";
+import useFollowingCelebQuery from "@/hooks/api/my-fund/useFollowingCelebQuery.js";
 
 function MyFollowingList() {
-  const sonnyCelebInfo = {
-    profileUrl:
-      "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202308/13/3756de8c-1ea6-4988-b063-25f26d9b76d5.jpg",
+  const { data } = useFollowingCelebQuery();
 
-    celebId: "sonny",
-    celebName: "손흥민",
-    followerNum: 1000,
-    isFollowing: false,
-  };
   return (
     <GridTemplate>
-      {new Array(6).fill(sonnyCelebInfo).map((info, index) => (
+      {data?.map((celeb, index) => (
         <SimpleCelebCard
           key={index}
-          profileUrl={info.profileUrl}
-          celebId={info.celebId}
-          celebName={info.celebName}
-          followerNum={info.followerNum}
-          isFollowing={info.isFollowing}
+          profileUrl={celeb.profileUrl}
+          celebId={celeb.celebId}
+          celebName={celeb.celebName}
+          followerNum={celeb.followerNum}
         />
       ))}
     </GridTemplate>
