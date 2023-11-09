3c475a58c607472807bba5ba8717acd5ee6a3079
Klomachenko <leekyumin0901@naver.com>
2023-11-10 02:43:34 +0900

    feat: SimpleCelebList 컴포넌트 구현

---
+++ src/src/components/celebrity-list/SimpleCelebList.jsx
@@ -0,0 +1,33 @@
+import { GridTemplate } from "@/styles/CommonStyle";
+import SimpleCelebCard from "../celebrity/SimpleCelebCard.jsx";
+
+function SimpleCelebList() {
+  const sonnyCelebInfo = {
+    celebId: 1,
+    celebName: "손흥민",
+    profileUrl:
+      "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202308/13/3756de8c-1ea6-4988-b063-25f26d9b76d5.jpg",
+    fundInProgressNum: 30,
+    totalFundMoney: 35000000,
+    followerNum: 10000,
+    isFollowing: false,
+    rank: 1,
+  };
+
+  return (
+    <GridTemplate>
+      {new Array(3).fill(sonnyCelebInfo).map((info, index) => (
+        <SimpleCelebCard
+          key={index}
+          profileUrl={info.profileUrl}
+          celebId={info.celebId}
+          celebName={info.celebName}
+          followerNum={info.followerNum}
+          isFollowing={info.isFollowing}
+        />
+      ))}
+    </GridTemplate>
+  );
+}
+
+export default SimpleCelebList;
