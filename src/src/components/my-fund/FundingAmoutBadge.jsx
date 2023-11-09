6090f11844d33f10a287164cb0ba18efb6642793
sihyonn <sihyeon982@naver.com>
2023-11-10 00:58:58 +0900

    feat: 후원한 펀딩목록에 FundingAmountBadge 추가

--- src/src/components/my-fund/FundingAmoutBadge.jsx
+++ src/src/components/my-fund/FundingAmoutBadge.jsx
@@ -17,21 +17,21 @@ const Styled = {
 
 /**
  * My펀딩페이지 - 펀딩내역 카드에 표시되는 내가 펀딩한 금액 뱃지
- * @params {string | number} 펀딩한 금액
+ *  @param {string || number} paymentAmount 후원한 펀딩 금액
  */
 
-function FundingAmountBadge({ fundingAmount = 500000 }) {
+function FundingAmountBadge({ paymentAmount }) {
   return (
     <Styled.Container>
       <Styled.Amount>
-        {Number(fundingAmount).toLocaleString("ko-KR")}원
+        {Number(paymentAmount).toLocaleString("ko-KR")}원
       </Styled.Amount>
     </Styled.Container>
   );
 }
 
 FundingAmountBadge.PropTypes = {
-  fundingAmount: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  paymentAmount: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 export default FundingAmountBadge;
