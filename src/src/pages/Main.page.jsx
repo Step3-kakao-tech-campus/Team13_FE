21b5c7f6f144c64ce7fec488471a4d1cf92b92a9
Klomachenko <leekyumin0901@naver.com>
2023-11-09 15:44:00 +0900

    feat: FundInfo Data 삭제

--- src/src/pages/Main.page.jsx
+++ src/src/pages/Main.page.jsx
@@ -15,24 +15,6 @@ const Styled = {
 };
 
 function MainPage() {
-  const fundInfo = {
-    fundId: 1,
-    fundTitle:
-      "손흥민 주장된 기념 지하철 광고 🎉🎉 축구중독자가 책임지고 펀딩합니다 ❤️‍🔥",
-    thumbnailUrl:
-      "https://ichef.bbci.co.uk/news/640/cpsprodpb/4118/production/_119546661_gettyimages-1294130887.jpg",
-    targetDate: "2023-12-17",
-    targetMoney: "3000000",
-    currentMoney: "2340000",
-    celebrityId: "sonny",
-    celebrityName: "손흥민",
-    celebrityProfileUrl:
-      "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202308/13/3756de8c-1ea6-4988-b063-25f26d9b76d5.jpg",
-    organizerId: "soccer123",
-    organizerName: "축구도사",
-    isInUserWishList: true,
-  };
-
   const sonnyCelebInfo = {
     celebId: 1,
     celebName: "손흥민",
