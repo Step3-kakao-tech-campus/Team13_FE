5533aab6e6c16f59c4893af9ddd79e7140853a58
sihyonn <sihyeon982@naver.com>
2023-11-09 14:08:19 +0900

    feat: 팔로잉 탭 내용 추가

--- src/src/components/my-fund/MyFollowingList.jsx
+++ src/src/components/my-fund/MyFollowingList.jsx
@@ -1,5 +1,30 @@
+import { GridTemplate } from "@/styles/CommonStyle";
+import SimpleCelebCard from "../celebrity/SimpleCelebCard.jsx";
+
 function MyFollowingList() {
-  return <div>MyFollowingList</div>;
+  const sonnyCelebInfo = {
+    profileUrl:
+      "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202308/13/3756de8c-1ea6-4988-b063-25f26d9b76d5.jpg",
+
+    celebId: "sonny",
+    celebName: "손흥민",
+    followerNum: 1000,
+    isFollowing: false,
+  };
+  return (
+    <GridTemplate>
+      {new Array(6).fill(sonnyCelebInfo).map((info, index) => (
+        <SimpleCelebCard
+          key={index}
+          profileUrl={info.profileUrl}
+          celebId={info.celebId}
+          celebName={info.celebName}
+          followerNum={info.followerNum}
+          isFollowing={info.isFollowing}
+        />
+      ))}
+    </GridTemplate>
+  );
 }
 
 export default MyFollowingList;
