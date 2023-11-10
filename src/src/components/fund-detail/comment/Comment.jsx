34438ce457f4c07a6e27866aa0826b0ce4ff014e
j8won <joowoni99@gmail.com>
2023-11-10 15:24:50 +0900

    feat: 펀딩 상세 페이지 댓글 컴포넌트 생성

--- src/src/components/fund-detail/comment/Comment.jsx
+++ src/src/components/fund-detail/comment/Comment.jsx
@@ -1,14 +1,49 @@
 import styled from "styled-components";
-import CommentInput from "@/components/fund-detail/comment/CommentInput.jsx";
+import TestAccountIcon from "@/assets/icon/TestAccountIcon";
 
-const Styled = {};
+const Styled = {
+  Container: styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+  `,
+
+  UserProfileBox: styled.div`
+    padding-right: 0.75rem;
+
+    img {
+      width: 2.25rem;
+      height: 2.25rem;
+      border-radius: 9999px;
+      object-fit: cover;
+    }
+  `,
+
+  TextWrapper: styled.div``,
+
+  ButtonWrapper: styled.div``,
+};
 
 function Comment() {
+  const profileUrl =
+    "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202308/13/3756de8c-1ea6-4988-b063-25f26d9b76d5.jpg";
+
   return (
-    <>
-      <CommentInput />
-      <div>d</div>
-    </>
+    <Styled.Container>
+      <Styled.UserProfileBox>
+        {profileUrl ? (
+          <img src={profileUrl} alt={`프로필 사진`} />
+        ) : (
+          <TestAccountIcon />
+        )}
+      </Styled.UserProfileBox>
+
+      <div>
+        <Styled.TextWrapper>d</Styled.TextWrapper>
+        <Styled.ButtonWrapper>ss</Styled.ButtonWrapper>
+      </div>
+    </Styled.Container>
   );
 }
+
 export default Comment;
