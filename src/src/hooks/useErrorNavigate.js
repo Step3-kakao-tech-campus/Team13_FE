2558c13fe7f577dd6dd8076e2c0a686889932ddd
j8won <joowoni99@gmail.com>
2023-11-13 17:30:03 +0900

    fix: localStorage 대신 useAtomValue 사용

--- src/src/hooks/useErrorNavigate.js
+++ src/src/hooks/useErrorNavigate.js
@@ -1,11 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import routes from "@/constants/routes.js";
+import { useAtomValue } from "jotai";
+import accessTokenAtom from "@/storage/accessToken.atom.js";
 
 function useErrorNavigate() {
+  const accessToken = useAtomValue(accessTokenAtom);
   const navigate = useNavigate();
 
   const handleError = (error) => {
-    if (!localStorage.getItem("accessToken")) {
+    console.log(accessToken);
+    if (!accessToken) {
       navigate(routes.signIn);
     }
   };
