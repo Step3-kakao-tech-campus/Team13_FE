ad098fda527ac761cfe39a72888a97ec5ba0efe3
j8won <joowoni99@gmail.com>
2023-11-17 21:57:09 +0900

    feat: 마이 펀딩 출금 승인, 거절 api data => params로 변경

--- src/src/hooks/api/my-fund/usePostWithdrawalRejectionMutation.js
+++ src/src/hooks/api/my-fund/usePostWithdrawalRejectionMutation.js
@@ -1,9 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import API from "@/constants/API.js";
 import myFundAPI from "@/api/myFundAPI";
 import { toast } from "react-hot-toast";
 
 function usePostWithdrawalRejectionMutation(handleSuccess) {
+  const queryClient = useQueryClient();
   return useMutation(
     [API.MY_FUND.REJECTION],
     async ({ postId, withdrawalId }) => {
@@ -16,6 +17,7 @@ function usePostWithdrawalRejectionMutation(handleSuccess) {
       onSuccess: () => {
         toast.success("출금승인이 거부되었습니다!");
         handleSuccess();
+        queryClient.invalidateQueries({ queryKey: [API.MY_FUND.WITHDRAWAL] });
       },
     },
   );
